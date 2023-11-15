561fd0e408497149acf1574c2236820884c440de
Ellen-314 <pilache.ellen@gmail.com>
2023-11-15 15:34:26 +0100

    hello

--- utilities.py
+++ utilities.py
@@ -11,8 +11,14 @@ class IFT():
         if x < self.a:
             return 0
         if x >= self.a and x <= self.b:
+            return self.h * ((x - self.a)/(self.b - self.a))
+        if x >= self.b and x <= self.c:
             return self.h
-        
+        if x >= self.c and x <= self.d:
+            return self.h * ((self.d - x)/(self.d - self.c))
+        if x > self.d:
+            return 0
+
     def __str__(self):
         return f" {label} : ({a}, {b}, {c}, {d})"
     
